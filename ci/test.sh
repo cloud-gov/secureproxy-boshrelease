f681d65f5640adc18c14558ccddfe5a2275f0cd1
Mark Boyd <mark.boyd@gsa.gov>
2023-03-03 15:11:53 -0500

    get libcidr from ubuntu package repository

--- ci/test.sh
+++ ci/test.sh
@@ -4,13 +4,7 @@ set -eux
 
 apt update
 export PERL_MM_USE_DEFAULT=1
-apt install -y build-essential && cpan -T -i Test::Nginx
-
-tar xvf libcidr-tarball/libcidr-1.2.3.tar.xz
-pushd libcidr-1.2.3
-  make
-  make install
-popd
+apt install -y build-essential libcidr-dev && cpan -T -i Test::Nginx
 
 release_path=$(pwd)/secureproxy-release-git-repo
 export LUA_PATH="${LUA_PATH:-};${release_path}/src/lua-libcidr-ffi/lib/?.lua"
