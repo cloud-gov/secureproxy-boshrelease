fde3bbe9f6b0c7959e03f3004a2c17cec553bdda
Mark Boyd <mark.boyd@gsa.gov>
2024-06-24 18:01:03 -0400

    add more test endpoints to sample node app

--- docker/node/web.js
+++ docker/node/web.js
@@ -8,6 +8,14 @@ http.createServer( (req, res) => {
   if (url === "/foo") {
     res.writeHead(204);
     res.end();
+  } else if (url === "/bar") {
+    res.writeHead(304);
+    res.end();
+  } else if (url === "/html") {
+    res.writeHead(200, undefined, {
+      'Content-Type': 'text/html'
+    });
+    res.end('<h1>foobar</h1>');
   } else {
     res.writeHead(200);
     res.end(`Hello World from NodeJS on port ${port} from container ${os.hostname()}`);
